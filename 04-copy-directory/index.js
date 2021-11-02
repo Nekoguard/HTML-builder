11e660a37036e487357a48f4587b243122eff5fe
Nekoguard <nekoguard@protonmail.com>
2021-11-02 20:11:06 +0300

    feat: implement 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const copyPath = path.join(__dirname, 'files-copy');
 
+function copyDir() {
+  // если папки ещё не существует - создаем её
+
+  fs.stat(copyPath, function(err) {
+    if (err && err.code === 'ENOENT') {
+      fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+
+    // создаем/обновляем содержимое папки
+
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+      if (err) {
+        throw err;
+      }
+
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file), path.join(copyPath, file), (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+
+      console.log('Done!');
+    });
+  });
+}
+
+copyDir();
